acb2fa7ea25d918a7ec0a5ab4c398a334e15a987
Afjal Ahamad <afjal.ahamad44@gmail.com>
2021-02-01 06:29:32 +0530

    first commit

---
+++ MyPortfolioProject/base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home,name='home'),
+    path('posts/', views.posts,name='posts'),
+    path('post/<slug:slug>', views.post,name='post'),
+    path('profile/', views.profile,name='profile'),
+    #CRUD PATHS
+    path('create_post',views.createPost,name='create_post'),
+    path('update_post/<slug:slug>/',views.upatePost,name='update_post'),
+    path('delete_post/<slug:slug>/',views.deletePost,name='delete_post'),
+    path('send_email/',views.sendEmail,name='send_email'),
+]
